f4a775d9a37b3a40338aaa031baa4ce4cefc1370
Felix Melligan <felix@lightbitslabs.com>
2023-05-08 19:03:41 -0400

    Add force delete to bucket config

--- aws/functionality/s3_bucket.tf
+++ aws/functionality/s3_bucket.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "config" {
   bucket = "lb-demo-config-bucket-${random_id.deployment_code.hex}"
+  force_destroy = true // Destroy the bucket if not empty
 
   tags = var.tags
 }
